904515600b770feabf969bf8feae85e2b3680ac5
Richard <richard1xur@uchicago.edu>
2022-03-13 23:31:51 -0500

    first commit

---
+++ detection.py
@@ -0,0 +1,14 @@
+import numpy as np
+from collections import Counter
+
+import cv2
+from imageio import imread
+
+# 1. figure out how to extract interest point operators
+# 2. figure out how to compute the corresponding SIFT descriptors.
+# 3. Implement robust matching of SIFT descriptors
+
+
+def lookup (reference_image):
+    
+    return None
